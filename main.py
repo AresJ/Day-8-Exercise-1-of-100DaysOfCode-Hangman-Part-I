b32d65c607d73e5dd36868744fd3275a1edafef1
Carlil Jean <jaedagk@live.com>
2022-04-01 16:39:56 +0000

    Initial commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #Write your code below this line 👇
-import Math
-def print_calc(test_h, test_w, coverage):
+import math
+def print_calc(height, width, cover):
+  return math.ceil(results = (height * width)/cover)))
   
 #Write your code above this line 👆
 # Define a function called paint_calc() so that the code below works.
